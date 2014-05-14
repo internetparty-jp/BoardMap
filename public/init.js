58d5d48f080df206352f40a931f69239c7395d70
harada-hiroshi <harada@orange-soft.jp>
2014-05-15 00:50:45 +0900

    no message

--- public/init.js
+++ public/init.js
@@ -2,12 +2,12 @@
 // ユーザー設定値
 //=============================================================================
 SETTING={
+CAT_URL:"/issue_categories.json"
+,ISSU_URL:"/issues.json"
+    
+,CAT_URL:"http://192.168.24.21:8090/Bordmap_debug_proxy/issue_categories.json.php"//debug
+,ISSU_URL:"http://192.168.24.21:8090/Bordmap_debug_proxy/issues.json.php"//debug
 
-//CAT_URL="/issue_categories.json";
-//,ISSU_URL="/issues.json";
-
-CAT_URL:"/debug_proxy/issue_categories.json.php"//debug
-,ISSU_URL:"/debug_proxy/issues.json.php"//debug
 ,ISSU_LIMIT:100//1回のリクエスト件数の上限
 ,ZOOM_LEVEL:15
 ,MAXZOOM:19
@@ -16,7 +16,5 @@ CAT_URL:"/debug_proxy/issue_categories.json.php"//debug
 ,DEFAULT_LNG:139.7805888205567
 ,TWEET_FORMAT:'@testposterdone <$subject$> #テストポスター祭り #testhash'
 ,POS_DATA_REV_URL:'test_rev.php'//twitter共有時にバックグラウンドで別のアドレスに掲示板データを投げる
-
-
 };
 
